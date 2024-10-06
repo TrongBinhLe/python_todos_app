e2108f734dc567bd987339211bfc3fb7615c3c16
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-10-06 10:15:28 +0700

    Create multi column on webpage via streamlit

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images\photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+    Hi, I'm TRBINH! I am Python programer, teacher, and founder of Python Home. I have worked companies from
+    various countries, such as Center
+    """
+    st.info(content)
