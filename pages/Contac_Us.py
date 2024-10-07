1f1e8f44ff9c8dec56f4cf5233050d2e28a903a3
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-10-07 13:02:17 +0700

    Sending email with stmplib on d23

--- pages/Contac_Us.py
+++ pages/Contac_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+      user_emails = st.text_input("Your email address")
+      message = st.text_area("Your message")
+      button_submit = st.form_submit_button("Submit")
+      if button_submit:
+            print("Press button submit")
+            print(button_submit)
