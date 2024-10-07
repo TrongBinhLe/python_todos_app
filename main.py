3318c31f6b56cbaac20a105e040fc6cde8746853
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-10-07 10:23:58 +0700

    Add app description, image, and links on the day22

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
+col1 ,empty_col, col2 = st.columns([1.8, 0.5, 1.8])
 
 with col1:
     st.image("/Users/admin/Documents/Others/python-projects/TodoListApp/python_todos_app/images/photo.png")
 
 with col2:
-    st.title("Ardit Sulce dasfasf")
+    st.title("Ardit Sulce")
     content = """
     Hi, I'm TRBINH! I am Python programer, teacher, and founder of Python Home. I have worked companies from
     various countries, such as the Center for Convervation Geography, to map and understand Australian ecosystem, image processing
@@ -21,13 +21,18 @@ st.write(content)
 
 df = pandas.read_csv("data.csv",sep=";")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
        st.header(row["title"])
-
+       st.write(row["description"])
+       st.image(f'/Users/admin/Documents/Others/python-projects/TodoListApp/python_todos_app/images/{index + 1}.png')
+       st.write(f'[Source code]({row["url"]})')
+    
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image(f'/Users/admin/Documents/Others/python-projects/TodoListApp/python_todos_app/images/{index + 1}.png')
+        st.write(f'[Source code]({row["url"]})')
